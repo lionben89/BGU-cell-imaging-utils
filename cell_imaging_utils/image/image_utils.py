fix min-max normalization

cell_imaging_utils/image/image_utils.py
@@ -67,7 +67,8 @@ class ImageUtils:
     """
     @staticmethod
     def normalize(image_ndarray,max_value=255,dtype=np.uint8) -> np.ndarray:
-        return ((image_ndarray/np.max(image_ndarray))*max_value).astype(dtype)
+        temp_image = image_ndarray-np.min(image_ndarray)
+        return (((temp_image)/np.max(temp_image))*max_value).astype(dtype)
     
     
     # @staticmethod

cell_imaging_utils/plots/plot_utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 
 
+
 log = logging.getLogger(__name__)
 
 """
@@ -22,8 +23,12 @@ class PlotsUtils:
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(title)
+        # plt.colorbar()
+        plt.imshow(image, cmap='seismic', interpolation='nearest')
+        # cb = np.linspace(0.0,1.0,20)
+        # cax = plt.axes(cb)
         plt.colorbar()
-        plt.imshow(image, cmap='hot', interpolation='nearest')
         if (save != None):
             plt.savefig(save)
+        plt.show()
 